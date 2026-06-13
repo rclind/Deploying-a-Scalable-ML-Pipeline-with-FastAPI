c2f26a9782cb51e85ec43ab9b921d104be210329
rclind <rclind@users.noreply.github.com>
2026-06-13 20:26:09 +0000

    Running API and fixing indentation in main.py

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ async def post_inference(data: Data):
         "sex",
         "native-country",
     ]
-     data_processed, _, _, _ = process_data(
+    data_processed, _, _, _ = process_data(
         data,
         categorical_features=cat_features,
         training=False,
