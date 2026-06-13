a25fa440b8ad44623dd665a5d638ebd80c071114
rclind <rclind@users.noreply.github.com>
2026-06-13 20:10:28 +0000

    Creating Restful API

--- main.py
+++ main.py
@@ -26,21 +26,18 @@ class Data(BaseModel):
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
     native_country: str = Field(..., example="United-States", alias="native-country")
 
-path = None # TODO: enter the path for the saved encoder 
+path = "model/encoder.pkl"
 encoder = load_model(path)
 
-path = None # TODO: enter the path for the saved model 
+path = os.path.join("model", "model.pkl")
 model = load_model(path)
 
-# TODO: create a RESTful API using FastAPI
-app = None # your code here
+app = FastAPI()
 
-# TODO: create a GET on the root giving a welcome message
 @app.get("/")
 async def get_root():
-    """ Say hello!"""
-    # your code here
-    pass
+    """Say hello!"""
+    return {"message": "Welcome to the Census Income Prediction API"}
 
 
 # TODO: create a POST on a different path that does model inference
@@ -64,11 +61,12 @@ async def post_inference(data: Data):
         "sex",
         "native-country",
     ]
-    data_processed, _, _, _ = process_data(
-        # your code here
-        # use data as data input
-        # use training = False
-        # do not need to pass lb as input
+     data_processed, _, _, _ = process_data(
+        data,
+        categorical_features=cat_features,
+        training=False,
+        encoder=encoder,
     )
-    _inference = None # your code here to predict the result using data_processed
+
+    _inference = inference(model, data_processed)
     return {"result": apply_label(_inference)}
